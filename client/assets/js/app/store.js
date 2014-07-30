f0648e19831514530378b7a430c10b3810285635
Matt Eby <eby.matthew@gmail.com>
2014-07-30 21:03:36 +0000

    using firebase adapter

--- client/assets/js/app/store.js
+++ client/assets/js/app/store.js
@@ -6,7 +6,7 @@ App.ApplicationSerializer = DS.RESTSerializer.extend({
 	primaryKey: '_id',
 });*/
 
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-  host: 'https://pager-c9-ebymatthew.c9.io/api'
+App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase('https://pagerlist.firebaseio.com')
 });
 
