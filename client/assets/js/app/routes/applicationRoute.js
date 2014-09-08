acaeb8e9bea69374c275bed6d8b1f0eaaa7fc815
Matt Eby <eby.matthew@gmail.com>
2014-09-08 03:30:07 +0000

    auth and unauth with firebase when simple auth authenticates or unauthenticates, respectivly

--- client/assets/js/app/routes/applicationRoute.js
+++ client/assets/js/app/routes/applicationRoute.js
@@ -2,4 +2,31 @@
 */
 
 // use the provided mixins in the application route
-App.ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin);
+App.ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin).extend({
+    
+    actions: {
+        sessionAuthenticationSucceeded: function() {
+            var AUTH_TOKEN = this.get('session.access_token');
+            var thisObj = this;
+            
+            console.log("sessionAuthenticationSucceeded");
+            App.FB.auth(AUTH_TOKEN, function(error) {
+              if(error) {
+                thisObj.invalidateSession();
+              } else {
+                var attemptedTransition = thisObj.get('session.attemptedTransition');
+                if (attemptedTransition) {
+                  attemptedTransition.retry();
+                  thisObj.set('session.attemptedTransition', null);
+                } else {
+                  thisObj.transitionTo('index');
+                }
+              }
+            });
+        },
+        sessionInvalidationSucceeded: function() {
+            App.FB.unauth();
+            window.location.replace('index.html');
+        }
+    }
+});
