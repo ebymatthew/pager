8e89a4183475067240d63e2edcb730f9b494badc
Matt Eby <eby.matthew@gmail.com>
2014-09-23 03:29:11 +0000

    prevent deletion of account

--- client/assets/js/app/controllers/account.js
+++ client/assets/js/app/controllers/account.js
@@ -1,16 +1,6 @@
 /* /controllers/account.js 
 */
-App.AccountController = Ember.ObjectController.extend({
-  actions: {
-    delete: function(){
-      // this tells Ember-Data to delete the current account
-      this.get('model').deleteRecord();
-      this.get('model').save();
-      // and then go to the accounts route
-      this.transitionToRoute('accounts');
-    }
-  }
-});
+App.AccountController = Ember.ObjectController;
 
 
 //Ember.ArrayController.extend({
